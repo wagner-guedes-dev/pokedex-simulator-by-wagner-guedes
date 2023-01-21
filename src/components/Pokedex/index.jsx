3c1e6e4b67f39fc922b6b7c7e5f781ecbb52e5b0
Wagner Guedes <97996373+wagner-guedes-dev@users.noreply.github.com>
2023-01-21 19:58:30 -0300

    update

--- src/components/Pokedex/index.jsx
+++ src/components/Pokedex/index.jsx
@@ -4,6 +4,11 @@ import axios from 'axios'
 
 import './pokedex.css'
 
+//tipo dos pokemons
+import { AiFillFire, AiFillBug } from 'react-icons/ai' 
+import { FaRegDotCircle, FaDragon, FaGhost, FaWater } from 'react-icons/fa'
+import { MdDarkMode } from 'react-icons/md'
+import { GiElectric, GiFairyWand, GiPunch,GiFluffyWing, GiHighGrass, GiGroundbreaker, GiIceBolt, GiPoisonGas, GiPsychicWaves, GiFallingRocks,GiSteelClaws } from 'react-icons/gi'
 const Pokedex = () => {
 
   const [allPokemons, setAllPokemons] = useState([])
@@ -11,15 +16,42 @@ const Pokedex = () => {
   const [idPokemon, setIdPokemon] = useState(0)
   const [atributPokemonOne, setAtributPokemonOne] = useState('')
   const [atributPokemonTwo, setAtributPokemonTwo] = useState('')
+  const [test, setTest] = useState([])
+  const [nick, setNick] = useState()
+
+
+  const tipoPokemons = {
+    bug: false, 
+    dark: false, 
+    dragon: false, 
+    electric: false, 
+    fairy: false, 
+    fighting: false,
+    fire: false, 
+    flying: false, 
+    ghost: false, 
+    grass: false, 
+    ground: false,
+    ice: false, 
+    normal: false, 
+    poison: false, 
+    psychic: false, 
+    rock: false, 
+    steel: false, 
+    water: false
+}
+  const [iconTipoPokemon, setIconTipoPokemon] = useState(tipoPokemons)
+  
 
   
   
 
   useEffect(()=>{
     
-    axios.get('https://pokeapi.co/api/v2/pokemon?limit=905&offset=0')
+    axios.get('https://pokeapi.co/api/v2/pokemon?limit=500&offset=0')
     .then((res)=>{
       setAllPokemons(res.data.results)
+      setTest(res.data.results)
     }).catch((err)=>{
       console.log(err)
     })
@@ -28,16 +60,46 @@ const Pokedex = () => {
   useEffect(()=>{
    
       axios.get(`https://pokeapi.co/api/v2/pokemon/${1 + idPokemon}`).then((res)=> {
-        setDetailsPokemons(res.data.sprites.other.dream_world.front_default)
+        setDetailsPokemons(res.data.sprites.front_default)
         setAtributPokemonOne(res.data.types[0].type.name)
         setAtributPokemonTwo('') //para pokemons que so tem um atributo
-        setAtributPokemonTwo(res.data.types[1].type.name)
+        if(res.data.types.length === 2){
+          setAtributPokemonTwo(res.data.types[1].type.name)
+        }
         
+        iconTipoPokemon.bug = false
+        iconTipoPokemon.dark = false
+        iconTipoPokemon.dragon = false
+        iconTipoPokemon.electric = false
+        iconTipoPokemon.fairy = false
+        iconTipoPokemon.fighting = false
+        iconTipoPokemon.fire = false
+        iconTipoPokemon.flying = false
+        iconTipoPokemon.ghost = false
+        iconTipoPokemon.grass = false
+        iconTipoPokemon.ground = false
+        iconTipoPokemon.ice = false
+        iconTipoPokemon.normal = false
+        iconTipoPokemon.poison = false
+        iconTipoPokemon.psychic = false
+        iconTipoPokemon.rock = false
+        iconTipoPokemon.steel = false
+        iconTipoPokemon.water = false
+        
+        
+        
+        
+        
+        
+
       })
       
+
   },[idPokemon])
 
-  
+  iconTipoPokemon[atributPokemonOne] = true
+  iconTipoPokemon[atributPokemonTwo] = true
+        
 
   
 
@@ -45,11 +107,14 @@ const Pokedex = () => {
     setIdPokemon(key)
   }
 
+  // colocando icons na escrita do tipo dos pokemons
+  
   
 
 
   return (
     <div id="pokedex">
+      
   <div id="left">
     <div id="logo"></div>
     <div id="bg_curve1_left"></div>
@@ -74,10 +139,32 @@ const Pokedex = () => {
         <div id="buttontopPicture2"></div>
       </div>
       <div id="picture">
-        <p>{atributPokemonOne}</p>
-        <p>{atributPokemonTwo}</p>
+        <div className='tipoIcon'>
+          {iconTipoPokemon.fire ? (<AiFillFire color='red' size={20}/>) : null} {/* fire */ } 
+          {iconTipoPokemon.normal ? (<FaRegDotCircle color='#D3D3D3' size={20}/>) : null}{/* normal */ } 
+          {iconTipoPokemon.bug ? (<AiFillBug color='#208B20' size={20}/>) : null}{/* bug */ } 
+          {iconTipoPokemon.dark ? (<MdDarkMode color='#2F4F4F' size={20}/>) : null} {/* dark */ } 
+          {iconTipoPokemon.dragon ? (<FaDragon color='#483D8B' size={20}/>) : null}{/* dragon */ } 
+          {iconTipoPokemon.electric ? (<GiElectric color='#FFFF00' size={20}/>) : null} {/* electric */ } 
+          {iconTipoPokemon.fairy ? (<GiFairyWand color='#D02090' size={20}/>) : null}{/* fairy */ } 
+          {iconTipoPokemon.fighting ? (<GiPunch color='#8B4513' size={20}/>) : null} {/* fighting */ } 
+          {iconTipoPokemon.flying ? (<GiFluffyWing color='#8470FF' size={20}/>) : null}{/* flying */ }    
+          {iconTipoPokemon.ghost ? (<FaGhost color='#6A5ACD' size={20}/>) : null} {/* ghost */ } 
+          {iconTipoPokemon.grass ? (<GiHighGrass color='#9ACD32' size={20}/>) : null}{/* grass */ } 
+          {iconTipoPokemon.ground ? (<GiGroundbreaker color='#BDB76B' size={20}/>) : null} {/* ground */ } 
+          {iconTipoPokemon.ice ? (<GiIceBolt color='#87CEEB' size={20}/>) : null}{/* ice */ } 
+          {iconTipoPokemon.poison ? (<GiPoisonGas color='#6A5ACD' size={20}/>) : null} {/* poison */ } 
+          {iconTipoPokemon.psychic ? (<GiPsychicWaves color='#DA70D6' size={20}/>) : null}{/* psychic */ } 
+          {iconTipoPokemon.rock ? (<GiFallingRocks color='#556B2F' size={20}/>) : null} {/* rock */ } 
+          {iconTipoPokemon.steel ? (<GiSteelClaws color='#708090' size={20}/>) : null}{/* steel */ }  
+          {iconTipoPokemon.water ? (<FaWater color='#6495ED' size={20}/>) : null}{/* water */ }  
+        </div>
+        
         <img src={detailsPokemons}  height="150"/> 
-
+        
+        
+       
+        
       </div>
       <div id="speakers">
         <div className="sp"></div>
@@ -109,6 +196,7 @@ const Pokedex = () => {
   <div id="right">
     <div id="stats">
       <ul >
+        
 
         {allPokemons.map((poke, key)=>{
           return(
